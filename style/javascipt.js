d01fa2a84bc2e378482f438446433010b1da9f38
Peter Karanja M <annahirpeters@gmail.com>
2022-12-11 01:36:01 +0300

    updates

--- style/javascipt.js
+++ style/javascipt.js
@@ -6,3 +6,13 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
